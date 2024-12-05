141cb56a468a4f29b0d1e45e3eae5fd6fde4ed01
sisterofDC <90042818+sisterofDC@users.noreply.github.com>
2024-12-05 09:06:01 +0800

    继续生成模块修改

---
+++ src/test/groovy/officesetproject/AnnualSouvenirSpec.groovy
@@ -0,0 +1,18 @@
+package officesetproject
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class AnnualSouvenirSpec extends Specification implements DomainUnitTest<AnnualSouvenir> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
